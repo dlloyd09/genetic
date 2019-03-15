483899bf7414f24831c6244dea84be9e8db0c859
Doug Lloyd <lloyd@cs50.harvard.edu>
2019-03-15 15:29:30 -0400

    add starter code

---
+++ starter/genetic.py
@@ -0,0 +1,14 @@
+####################################
+# genetic.py                       #
+#                                  #
+# Script to run genetic algorithm  #
+#                                  #
+# Doug Lloyd                       #
+# March 15, 2019                   #
+# CS50                             #
+####################################
+
+import random
+
+from phrase import Phrase, target
+from helpers import summarize
---
+++ starter/phrase.py
@@ -0,0 +1,13 @@
+####################################
+# phrase.py                        #
+#                                  #
+# Defines Phrase class for genetic #
+#                                  #
+# Doug Lloyd                       #
+# March 15, 2019                   #
+# CS50                             #
+####################################
+
+import random
+
+target = "This is CS50 on Twitch!"
