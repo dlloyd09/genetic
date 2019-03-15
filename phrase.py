27c79aabd0f06a7faf4b8d3b05d5e75629f7e5c0
Doug Lloyd <lloyd@cs50.harvard.edu>
2019-03-15 14:51:55 -0400

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,42 +1,38 @@
 import random
+from cs50 import get_string
 
-from phrase import Phrase, target
-from helpers import summarize
-from cs50 import get_int
+target = get_string("What target do you want to match? ")
 
-popSize = get_int("How many individuals in each generation? ")
-population = []
-bestScore = 0
-generation = 1
-
-for i in range(popSize):
-    population.append(Phrase())
-
-while bestScore < len(target):
+class Phrase:
+    def __init__(self):
+        self.characters = []
+        for i in range(len(target)):
+            character = chr(random.choice(range(32, 127)))
+            self.characters.append(character)
     
-    for i in range(popSize):
-        population[i].getFitness()
+    def getContents(self):
+        return ''.join(self.characters)
+    
+    def getFitness(self):
+        self.fitness = 0
+        for i in range(len(self.characters)):
+            if self.characters[i] == target[i]:
+                self.fitness += 1
+    
+    def crossover(self, partner):
+        child = Phrase()
+        for i in range(len(self.characters)):
+            if random.random() < 0.5:
+                child.characters[i] = self.characters[i]
+            else:
+                child.characters[i] = partner.characters[i]
         
-        if population[i].fitness > bestScore:
-            bestScore = population[i].fitness
-            summarize(generation, population[i].getContents(), bestScore)
+        return child
     
-    matingPool = []
-    
-    parents = population[:]
-    population = []
-    
-    for i in range(popSize):
-        for j in range(parents[i].fitness):
-            matingPool.append(parents[i])
-    
-    for i in range(popSize):
-        x = random.choice(range(len(matingPool)))
-        y = random.choice(range(len(matingPool)))
-        
-        child = matingPool[x].crossover(matingPool[y])
-        child.mutate(generation)
-        
-        population.append(child)
-
-    generation += 1
+    def mutate(self, generation):
+        for i in range(len(self.characters)):
+            if generation > 100:
+                mrate = random.random()
+                mrate *= (generation / 100)
+                if mrate < 0.01:
+                    self.characters[i] = chr(random.choice(range(32, 127)))
